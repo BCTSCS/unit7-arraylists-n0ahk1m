1cb111e8278ea60d2a22e92207fa323a5178dd6c
Noah Kim <noahmatiaskim@gmail.com>
2024-12-12 23:28:17 -0500

    part 3 and part 4

--- ListOne.java
+++ ListOne.java
@@ -17,8 +17,20 @@ public class ListOne{
         numbers.remove(numbers.size() - 1);  // pop() in Python
         numbers.remove(Integer.valueOf(1));   // remove(1) in Python
         numbers.remove(0);             // del numbers[0] in Python
-    }
 
-    ArrayList<Integer> nums = new ArrayList<>();
+        // Java -Use ListOne class and add below code Static typing with Generics
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.add(1);
+        nums.add("string");  
+        
+        // Java
+        nums.add(4);           // Add to end
+        int length =  nums.size();  // Get length
+
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        List<Integer> subset = numbers.subList(1, 4);  // [2, 3, 4]
+    }
     
+
+
 }
